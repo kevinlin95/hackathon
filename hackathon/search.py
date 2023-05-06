b5467236acaaab19b9b28f3f10da15692870c115
Loli-Killer <38392948+Loli-Killer@users.noreply.github.com>
2023-05-06 10:28:14 -0400

    Added several features
    - Added internship idea
    - Added error handling for invalid OpenAI api key
    - Added the switch to launch in either development or production mode

--- hackathon/search.py
+++ hackathon/search.py
@@ -1,5 +1,6 @@
 import openai
 from flask import Blueprint, redirect, render_template, request
+from openai.error import AuthenticationError
 
 from config import Config
 
@@ -14,6 +15,11 @@ services = {
     "Academic Advising": "https://www.laguardia.edu/Academic-Advising/",
     "Career Services": "https://www.laguardia.edu/Career-Services/"
 }
+
+internship_resources = [
+    ["LinkedIn", "https://www.linkedin.com/"],
+    ["LAGCC Job Postings", "https://www.laguardia.edu/careerservices/job-posting/"]
+]
 
 # Prompt to use for the ChatGPT API
 prompt = "I want you to act as a LaGuardia Community College mentor. You must be friendly and polite. Your job is to quickly guide students to the information and resources they are looking for. Please jump straight to the answer, and do not include any unnecessary phrases or introductions. Only respond to academic searches related to LaGuardia Community College. Otherwise, please respond with 'No available resources'."
@@ -30,15 +36,21 @@ def search():
         keyname = list(services.keys())[available_services.index(service.lower())]
         return redirect(services[keyname])
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt + "\nStudent question: " + service + "\n",
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
+    if "internship" in service.lower():
+        return render_template('search.html', answer="Here are some internship resources:", service_links=internship_resources)
+
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt + "\nStudent question: " + service + "\n",
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+    except AuthenticationError:
+        return render_template('error.html', error="Invalid API key for OpenAI. Please check your .env file.")
 
     # Extract the answer from the response
     answer: str = response.choices[0].text.strip()
@@ -50,6 +62,7 @@ def search():
             # If it does, add a link to the corresponding service
             keyname = list(services.keys())[available_services.index(word)]
             service_links.append((word.capitalize(), services[keyname]))
+    service_links = list(set(service_links))
 
     # Render the template with the answer and service links
-    return render_template('search.html', answer=answer, service_links=service_links)
+    return render_template('search.html', answer=answer, service_link_title=True, service_links=service_links)
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import logging
+
+import waitress
+from tap import Tap
+
 from hackathon import create_app
 
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
 
-app = create_app()
+
+class ServerType(Tap):
+    production: bool
+
+    def configure(self) -> None:
+        self.add_argument('--production', '-p', action='store_true', default=False,
+                          help='Whether to run the server in production mode')
+
+
 if __name__ == '__main__':
-    app.run()
+    args = ServerType().parse_args()
+    if args.production:
+        waitress.serve(create_app(), host='127.0.0.1', port=5000)
+    else:
+        create_app().run(host='127.0.0.1', port=5000, debug=True)
